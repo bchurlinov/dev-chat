f3e72e47cb42e573e20efb097100c7a6b311b001
bchurlinov <bchurlinov@hotmail.com>
2020-02-05 18:43:31 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/database";
 import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCcr13AZkolsXAV96UM1XRIy4w1wB7kIq0",
-    authDomain: "bojan-b3887.firebaseapp.com",
-    databaseURL: "https://bojan-b3887.firebaseio.com",
+    apiKey: "*****",
+    authDomain: "*****",
+    databaseURL: "*****",
     projectId: "bojan-b3887",
-    storageBucket: "bojan-b3887.appspot.com",
-    messagingSenderId: "112203988888",
-    appId: "1:112203988888:web:5ceadf01c7b3d93c3a75ed"
+    storageBucket: "*****",
+    messagingSenderId: "*****",
+    appId: "*****"
 };
 
 firebase.initializeApp(firebaseConfig);
